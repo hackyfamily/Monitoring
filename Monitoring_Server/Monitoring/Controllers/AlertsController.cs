d544954942fd3dc9d010f2de3b1e473bab4a439b
hackyfamily <hackyIS1>
2014-12-06 07:45:39 -0600

    Trigger alert

--- Monitoring_Server/Monitoring/Controllers/AlertsController.cs
+++ Monitoring_Server/Monitoring/Controllers/AlertsController.cs
@@ -40,5 +40,35 @@ namespace Monitoring.Controllers
 
             return RedirectToAction("Index", "Home");
         }
+
+        public ActionResult TriggerAlert()
+        {
+          // Send Alert - Heart Attack 
+            string recipient = WebConfigurationManager.AppSettings["Phone"];
+            string eventName = "Heart Rate";
+            string body = "Alert: Heart Rate > 100. Emergency Services Contact Number: 615.945.6528";
+
+            
+            string server = WebConfigurationManager.AppSettings["SMTPServer"];
+            int port = Int32.Parse(WebConfigurationManager.AppSettings["SMTPPort"]);
+            string username = WebConfigurationManager.AppSettings["SMTPUser"];
+            string password = WebConfigurationManager.AppSettings["SMTPPassword"];
+
+            SmtpClient client = new SmtpClient(server, port)
+            {
+                Credentials = new NetworkCredential(username, password),
+                EnableSsl = true
+            };
+
+            MailMessage mailMessage = new MailMessage(username, recipient);
+            mailMessage.IsBodyHtml = true;
+            mailMessage.Subject = string.Format("Alert for {0}", eventName);
+            mailMessage.Body = body;
+
+            client.Send(mailMessage);
+
+            return RedirectToAction("Index", "Home");
+        }
+
     }
 }

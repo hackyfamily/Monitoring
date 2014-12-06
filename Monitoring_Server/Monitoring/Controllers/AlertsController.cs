af7bc7e7e90e6e8a82a9ddff3f4aee929221d146
hackyfamily <hackyIS1>
2014-12-06 05:57:13 -0600

    Alert changes

--- Monitoring_Server/Monitoring/Controllers/AlertsController.cs
+++ Monitoring_Server/Monitoring/Controllers/AlertsController.cs
@@ -17,42 +17,12 @@ namespace Monitoring.Controllers
         public ActionResult Index()
         {
             return View();
-
-            /*
-            //string recipient = "2146862632@txt.att.net";  //Aaron's cellphone
-            string recipient = "aarauz15@gmail.com";
-            string eventName = "Heart Stop";
-            string body = "Test message, please ignore";
-
-            SendSMS(recipient, eventName, body);
-            */
         }
 
         [HttpPost]
         public ActionResult Index(FormCollection form)
         {
             return RedirectToAction("Index", "Home");
-        }
-
-        private void SendSMS(string recipient, string eventName, string body)
-        {
-            string server = WebConfigurationManager.AppSettings["SMTPServer"];
-            int port = Int32.Parse(WebConfigurationManager.AppSettings["SMTPPort"]);
-            string username = WebConfigurationManager.AppSettings["SMTPUser"];
-            string password = WebConfigurationManager.AppSettings["SMTPPassword"];
-
-            SmtpClient client = new SmtpClient(server, port)
-            {
-                Credentials = new NetworkCredential(username, password),
-                EnableSsl = true
-            };
-
-            MailMessage mailMessage = new MailMessage(username, recipient);
-            mailMessage.IsBodyHtml = true;
-            mailMessage.Subject = string.Format("Alert for {0}", eventName);
-            mailMessage.Body = body;
-
-            client.Send(mailMessage);
         }
 
     }

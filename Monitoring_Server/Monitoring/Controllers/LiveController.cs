ae57690a598f731d1dd6c78d3144ce6599b40e9e
Hernan Torrez <hernantorrez@gmail.com>
2014-12-06 01:43:30 -0400

    Commit

---
+++ Monitoring_Server/Monitoring/Controllers/LiveController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Monitoring.Controllers
+{
+    public class LiveController : Controller
+    {
+        private static List<int> _data = new List<int>{70, 75, 77, 76};
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult Data()
+        {
+            Random random = new Random();
+
+            int y = random.Next(70, 100);
+
+            _data.RemoveAt(0);
+            _data.Add(y);
+
+            return Json(_data.ToArray(), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Monitoring_Server/Monitoring/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Monitoring.MvcApplication" Language="C#" %>
